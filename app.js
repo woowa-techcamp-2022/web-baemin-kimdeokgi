9c70f12f26b52b9c789f6206cd1625e610ea60cc
deok2kim <dukgi77@naver.com>
2022-07-06 22:39:23 +0900

    Design: 약관동의(회원가입) 페이지 UI 작업

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
+const termsRouter = require('./routes/terms');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/terms', termsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
