377b5ca0170395f9fdeb41c9410afb825f16e423
deok2kim <dukgi77@naver.com>
2022-07-07 00:20:41 +0900

    Design: 휴대폰인증(회원가입) UI 작업

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const termsRouter = require('./routes/terms');
+const authPhoneRouter = require('./routes/authPhone');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/terms', termsRouter);
+app.use('/authPhone', authPhoneRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
