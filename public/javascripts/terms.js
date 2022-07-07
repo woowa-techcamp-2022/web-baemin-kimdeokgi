95388d10bbadd37d4e91f1d324eb977100e9c755
deok2kim <dukgi77@naver.com>
2022-07-07 14:21:56 +0900

    Feat: 약관 동의(회원가입) 기능 구현 [#4]

---
+++ public/javascripts/terms.js
@@ -0,0 +1,31 @@
+const $termAll = document.querySelector('#termAll');
+
+const validationTerms = () => {
+  const term_agree_count = [1,2,3].filter(idx => document.querySelector(`#term${idx}`).checked).length;
+  return term_agree_count === 3;
+}
+
+const toggleAllterms = () => {
+  const terms = document.querySelectorAll('input[name=term]');
+  terms.forEach(term => term.checked = $termAll.checked);
+}
+
+const $termsForm = document.querySelector('#termsForm');
+
+$termsForm.addEventListener('change', e => {
+  const $el = e.target
+  if ($el.name === 'term') {
+    if (!$el.checked) {
+      $termAll.checked = false;
+    } else {
+      const terms = document.querySelectorAll('input[name=term]');
+      let term_agree_count = 0;
+      terms.forEach(term => term.checked ? term_agree_count ++ : '')
+      term_agree_count === terms.length ? $termAll.checked = true : '';
+    }
+  } else if ($el.name = 'termAll') {
+    toggleAllterms()
+  }
+  document.querySelector('#next').disabled = !validationTerms();
+})
+
